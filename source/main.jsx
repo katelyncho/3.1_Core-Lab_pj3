bc1296a4cda760bc5e0cf73c7e643dfc9089393d
Minjung Cho <katelyn5897@gmail.com>
2025-11-07 13:13:32 -0500

    basic structure

--- source/main.jsx
+++ source/main.jsx
@@ -2,14 +2,44 @@ import { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 window.addEventListener("load", () => {
-  let root = createRoot(document.getElementById("root"));
+  const root = createRoot(document.getElementById("root"));
   root.render(<App />);
 });
 
 function App() {
-  // Your app code goes here...
-  return <p>(test)</p>;
+  const items = [
+    "cat.png",
+    "energydrink.png",
+    "food.png",
+    "friends.png",
+    "sleep.png",
+    "shower.png",
+  ];
 
-  return;
-  <div id="gridContainer"></div>;
+  return (
+    <div id="app">
+      <div id="characterPlace">
+        <img
+          id="characterImg"
+          src="img/character.png"
+          alt="character"
+          draggable="false"
+        />
+      </div>
+
+      <div id="gridContainer">
+        <img id="item1" src="img/cat.png" alt="cat" draggable="false" />
+        <img
+          id="item2"
+          src="img/energydrink.png"
+          alt="energydrink"
+          draggable="false"
+        />
+        <img id="item3" src="img/food.png" alt="food" draggable="false" />
+        <img id="item4" src="img/friends.png" alt="friends" draggable="false" />
+        <img id="item5" src="img/sleep.png" alt="sleep" draggable="false" />
+        <img id="item6" src="img/shower.png" alt="shower" draggable="false" />
+      </div>
+    </div>
+  );
 }
